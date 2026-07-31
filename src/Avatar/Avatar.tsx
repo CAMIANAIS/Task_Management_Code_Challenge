b4f034cbe96dc958c32eedd31df1cd0bdee6f1e8
CAMIANAIS <cmamanicr@unsa.edu.pe>
2026-07-31 16:12:34 -0500

    feat(avatar): add reusable Avatar component

---
+++ src/Avatar/Avatar.tsx
@@ -0,0 +1,24 @@
+import avatar from '../assets/avatar.png'
+import styles from './Avatar.module.css'
+
+const samplePhoto = avatar
+type typeAvatar = {
+    fullName: string
+    size: 'sm' | 'md' | 'lg'
+    avatar: string
+    showName: boolean
+}
+const dimensionBySize = {
+    sm: { width: 32, height: 32 },
+    md: { width: 40, height: 40 },
+    lg: { width: 48, height: 48 },
+}
+export function Avatar({ fullName, size, avatar, showName }: typeAvatar) {
+    const dimension = dimensionBySize[size]
+    const bordeRadius = { borderRadius: "50%" }
+    const combined = { ...dimension, ...bordeRadius }
+    return <div className={styles.profileInfo__card}>
+        <img src={avatar ? avatar : samplePhoto} alt={fullName} style={{ ...combined }} ></img>
+        {showName ? <p className={styles.profileInfo__text}> {fullName}</p > : ''}
+    </div >
+}
