29d9150d0fa57f6923c3d519bfbbbc5a61ee40d5
CAMIANAIS <cmamanicr@unsa.edu.pe>
2026-07-31 16:12:35 -0500

    feat(card): add Card component to render a single task

---
+++ src/Card/Card.tsx
@@ -0,0 +1,19 @@
+import type { Task } from '../Task/Task'
+import { Tag } from '../Tag/Tag'
+import { Avatar } from '../Avatar/Avatar'
+
+type CardProps = {
+    task: Task,
+}
+
+export function Card({ task }: CardProps) {
+    return <div>
+        <p>{task.name}</p>
+        <div>{task.tags.map((tag) => (
+            <Tag key={tag} tag={tag} />
+        ))}</div>
+        <p>{task.pointEstimate}</p>
+        <p>{task.dueDate}</p>
+        <div>{task.assignee ? <Avatar avatar={task.assignee.avatar} fullName={task.assignee.fullName} size='sm' showName={true} /> : <span>No assignee</span>}</div>
+    </div>
+}
