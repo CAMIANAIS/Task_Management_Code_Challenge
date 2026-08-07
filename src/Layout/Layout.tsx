72b9475c933b7b32ffb6af3cc1ab4b699906e093
CAMIANAIS <cmamanicr@unsa.edu.pe>
2026-08-06 19:55:50 -0500

    feat(search): add SearchContext and wire search bar to task list

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -2,17 +2,20 @@ import { Outlet } from 'react-router-dom'
 import { ApplicationSideBar } from '../ApplicationSidebar/ApplicationSidebar'
 import { TopNavigationBar } from '../TopNavigationBar/TopNavigationBar'
 import style from './Layout.module.css'
+import { SearchProvider } from '../SearchContext/SearchContext'
 
 export function Layout() {
     return (
-        <div className={style.layout__container}>
-            <ApplicationSideBar />
-            <div className={style.layout__right__section}>
-                <TopNavigationBar />
-                <div className={style.layout__content}>
-                    <Outlet />
+        <SearchProvider>
+            <div className={style.layout__container}>
+                <ApplicationSideBar />
+                <div className={style.layout__right__section}>
+                    <TopNavigationBar />
+                    <div className={style.layout__content}>
+                        <Outlet />
+                    </div>
                 </div>
             </div>
-        </div>
+        </SearchProvider>
     )
 }
--- src/TopNavigationBar/TopNavigationBar.tsx
+++ src/TopNavigationBar/TopNavigationBar.tsx
@@ -1,10 +1,12 @@
 import { Avatar } from "../Avatar/Avatar";
+import { useSearch } from "../SearchContext/SearchContext";
 import styles from "./TopNavigationBar.module.css";
 export function TopNavigationBar() {
+    const { searchTerm, setSearchTerm } = useSearch()
     return (
         <div className={styles.top_navigation__bar}>
             <img src="/navBarIcons/search.svg" alt="Search" className={styles.search__icon} />
-            <input type="text" placeholder="Search..." className={styles.search__input} />
+            <input type="text" placeholder="Search..." className={styles.search__input} value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
             <img src="/navBarIcons/notification.svg" alt="Notification" className={styles.notification__icon} />
             <Avatar avatar="" fullName="" size="sm" showName={false} />
         </div>
